35657f01a5bbfa8a5112ad7d7aacb07d71d932d3
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2016-02-16 23:00:39 +0000

    Use library file in the project.

--- package.js
+++ package.js
@@ -11,8 +11,8 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
-  api.addFiles('googlemaps-js-info-bubble.js');
+  api.versionsFrom('METEOR@1.0');
+  api.addFiles('js-info-bubble/src/infobubble-compiled.js', 'client');
 });
 
 Package.onTest(function(api) {
