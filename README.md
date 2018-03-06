28238281c4e01c1a0bf40ad02bdb559c96f55386
Ruslan Kabalin <r.kabalin@lancaster.ac.uk>
2018-03-06 17:48:47 +0000

    Prepare for publishing on GitHub.

---
+++ README.md
@@ -0,0 +1,3 @@
+# googlemaps-js-info-bubble
+
+Meteor package wrapper for [googlemaps/js-info-bubble](https://github.com/googlemaps/js-info-bubble).
--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
-  name: 'kabalin:googlemaps-js-info-bubble',
+  name: 'espressolocator:googlemaps-js-info-bubble',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'Meteor package wrapper for googlemaps/js-info-bubble.',
+  git: 'https://github.com/espressolocator/googlemaps-js-info-bubble',
   documentation: 'README.md'
 });
 
@@ -15,8 +11,3 @@ Package.onUse(function(api) {
   api.addFiles('js-info-bubble/src/infobubble-compiled.js', 'client');
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('kabalin:googlemaps-js-info-bubble');
-  api.addFiles('googlemaps-js-info-bubble-tests.js');
-});
